644c2df95e017ad03fa0ba050cd99e1d5742a9f0
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-03-29 22:36:05 -0600

    added arrays of all characters

--- script.js
+++ script.js
@@ -2,14 +2,36 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+var listUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var listLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var listNums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var listChars = ['!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', '\,', '-', '\.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+    passwordText.value = password;
+    console.log("here");
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+/* 
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+*/
